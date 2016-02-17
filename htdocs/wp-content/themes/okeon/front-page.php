9849f2db497b590e8c7e758dfb874f1fee4114e1
Julia Janicki <jhjanicki@gmail.com>
2016-02-17 15:44:58 +0900

    got menu to collapse on enter

--- htdocs/wp-content/themes/okeon/front-page.php
+++ htdocs/wp-content/themes/okeon/front-page.php
@@ -80,8 +80,19 @@
 
 <script>
 
+
+// $(function(){
+//     $("#menu-menu-1").hide();
+//     $("#menu-button").on("click", function(){
+//         $("#menu-menu-1").toggle();
+//     });
+// });
+
+
+
 (function($) {
 
+       
   $.fn.menumaker = function(options) {
       
       var cssmenu = $(this), settings = $.extend({
@@ -91,9 +102,10 @@
       }, options);
 
       return this.each(function() {
-      
         cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+        
         $(this).find("#menu-button").on('click', function(){
+        
           $(this).toggleClass('menu-opened');
           var mainmenu = $(this).next('ul');
           
@@ -126,19 +138,7 @@
         if (settings.format === 'multitoggle') multiTg();
         else cssmenu.addClass('dropdown');
 
-        if (settings.sticky === true) cssmenu.css('position', 'fixed');
-
-        resizeFix = function() {
-          if ($( window ).width() > 1024) {
-            cssmenu.find('ul').show();
-          }
-
-          if ($(window).width() <= 768) {
-            cssmenu.find('ul').hide().removeClass('open');
-          }
-        };
-        resizeFix();
-        return $(window).on('resize', resizeFix);
+        
 
       });
   };
