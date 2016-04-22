181e4b9f2022478be82fc9b2452748c8645c9837
Julia Janicki <jhjanicki@gmail.com>
2016-04-22 13:41:12 +0900

    font change and Japanese pages update

--- htdocs/wp-content/themes/okeon/functions.php
+++ htdocs/wp-content/themes/okeon/functions.php
@@ -12,6 +12,8 @@ function theme_styles(){
 	wp_enqueue_style('Roboto_css', 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
 	wp_enqueue_style('Open_Sans_css', 'https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic');
 	wp_enqueue_style('Raleway_css', 'https://fonts.googleapis.com/css?family=Raleway:400,900italic,900,800italic,800,700italic,700,600italic,600,500italic,500,400italic,300italic,300,200italic,200,100,100italic');
+	wp_enqueue_style('Tillium_css','https://fonts.googleapis.com/css?family=Titillium+Web:400,900,700,700italic,600italic,400italic,600,300italic,300,200italic,200');
+	wp_enqueue_style('Lato_lora_css','https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic|Lora:400,400italic,700,700italic');
 }
 
 add_action('wp_enqueue_scripts','theme_styles');
