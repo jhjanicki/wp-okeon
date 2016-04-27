058037e9fea6052b623b7302807dc6d5401ea644
Julia Janicki <jhjanicki@gmail.com>
2016-04-27 16:22:10 +0900

    carousel change

--- htdocs/wp-content/themes/okeon/front-page.php
+++ htdocs/wp-content/themes/okeon/front-page.php
@@ -50,6 +50,7 @@
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
   </ol>
   <div class="carousel-inner">
 			<?php if (have_posts()): while (have_posts()): the_post(); ?>
