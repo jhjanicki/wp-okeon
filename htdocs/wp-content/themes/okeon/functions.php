2929e7471e63059134dfc9e9d56a42b56a84083a
Julia Janicki <jhjanicki@gmail.com>
2016-10-28 13:57:42 +0900

    new basemap layers combining leaflet and cartodb
    Instead of using map box custom tiles and switching layers each time, I
    used leaflet basemaps and added overlays of data visualized in cartodb
    (i.e. roads, rivers)

--- htdocs/wp-content/themes/okeon/functions.php
+++ htdocs/wp-content/themes/okeon/functions.php
@@ -7,7 +7,8 @@ function theme_styles(){
 	wp_enqueue_style('jquery_css','http://www.jqueryscript.net/css/jquerysctipttop.css');
 	wp_enqueue_style('main_css', get_template_directory_uri().'/style.css');	
 	wp_enqueue_style('menu_css', get_template_directory_uri().'/css/menu.css');	
-	wp_enqueue_style('leaflet_css', get_template_directory_uri().'/js/libraries/leaflet/leaflet.css');	
+	// wp_enqueue_style('leaflet_css', get_template_directory_uri().'/js/libraries/leaflet/leaflet.css');
+	wp_enqueue_style('cartodb_css', 'http://libs.cartocdn.com/cartodb.js/v3/3.15/themes/css/cartodb.css');	
 	wp_enqueue_style('font_awesome_css', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
 	wp_enqueue_style('Roboto_css', 'https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
 	wp_enqueue_style('Open_Sans_css', 'https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic');
@@ -24,10 +25,12 @@ function theme_js(){
 	wp_enqueue_script('bootstrap_js','http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js',array('jquery'),'',true);
 	wp_enqueue_script('d3_js','http://d3js.org/d3.v3.min.js',array('jquery'),'',true);
 	wp_enqueue_script('d3_geo_js','http://d3js.org/d3.geo.projection.v0.min.js',array('jquery'),'',true);
+	wp_enqueue_script('queue_js','http://d3js.org/queue.v1.min.js',array('jquery'),'',true);
 	wp_enqueue_script('topojson_js',get_template_directory_uri().'/js/libraries/topojson-js/topojson.min.js',array('jquery','d3_js'),'',true);
 	//wp_enqueue_script('leaflet_js',get_template_directory_uri().'/js/libraries/leaflet/leaflet.js','','',true);
-	wp_enqueue_script('leaflet_js','http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js','','',true);
-	wp_enqueue_script('map_js',get_template_directory_uri().'/js/map-new.js',array('jquery','d3_js','d3_geo_js','topojson_js','leaflet_js'),'',true);
+	// wp_enqueue_script('leaflet_js','http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js','','',true);
+	wp_enqueue_script('cartodb_js','http://libs.cartocdn.com/cartodb.js/v3/3.15/cartodb.js','','',true);
+	wp_enqueue_script('map_js',get_template_directory_uri().'/js/map-new.js',array('jquery','d3_js','d3_geo_js','topojson_js','cartodb_js'),'',true);
 	wp_enqueue_script('front_map_js',get_template_directory_uri().'/js/front-map.js',array('jquery','d3_js','d3_geo_js','topojson_js','leaflet_js'),'',true);
 	wp_enqueue_script('menu_js',get_template_directory_uri().'/js/libraries/menu.js',array('jquery'),'',true);
 	wp_enqueue_script('scroll_js',get_template_directory_uri().'/js/scroll.js',array('jquery'),'',true);
